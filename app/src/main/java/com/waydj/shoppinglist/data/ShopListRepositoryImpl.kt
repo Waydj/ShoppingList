7cf61229f3e2002acc455877172f38bb0669d6b9
Waydj <farhminait@gmail.com>
2025-02-14 15:43:56 +0200

    создание activities, добавление linearLayout для проверки работы

--- app/src/main/java/com/waydj/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/waydj/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -8,12 +8,16 @@ import com.waydj.shoppinglist.domain.ShopListRepository
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 ->
+        o1.id.compareTo(
+            o2.id
+        )
+    })
 
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10){
+        for (i in 0 until 100) {
             val item = ShopItem("name $i", i, true)
             addShopItem(item)
         }
--- app/src/main/java/com/waydj/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/waydj/shoppinglist/presentation/MainActivity.kt
@@ -2,30 +2,64 @@ package com.waydj.shoppinglist.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.waydj.shoppinglist.R
+import com.waydj.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var linearLayoutShopList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById(R.id.main)
+        ) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        linearLayoutShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             Log.d("MainActivity", it.toString())
+            showList(it)
+        }
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        linearLayoutShopList.removeAllViews()
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(
+                layoutId,
+                linearLayoutShopList,
+                false
+            )
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            shopItem.count.toString().also { tvCount.text = it }
+            view.setOnLongClickListener {
+                viewModel.changeEnableState(shopItem)
+                true
+            }
+            linearLayoutShopList.addView(view)
         }
     }
 }
